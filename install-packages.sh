fdc2b8979b85c898d3bcee479df309d81b1454aa
Douglas Schonholtz <schonholtzd@gmail.com>
2020-08-25 19:47:12 -0400

    Fixing the install script to actually install what we need it to

--- install-packages.sh
+++ install-packages.sh
@@ -1,10 +1,18 @@
+# install all of the ppas for the packages we will install later
 while read p; do
-  add-apt-repository ppa:gnome-terminator "${p}"
+  add-apt-repository ppa:"${p}"
 done <ppas.txt
 
 apt update
 
+# Install the packages
 while read p; do
 	apt install "${p}"
 done <packages.txt
 
+# install neovim
+-u ${logname} mkdir -p ~/.config/nvim/
+-u ${logname} cp ./init.vim ~/.config/nvim/
+
+#install vim-plug for neovim
+-u ${logname} curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
