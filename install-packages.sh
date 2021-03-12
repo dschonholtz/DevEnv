9f9530778c3558d63959169425cbaf3cbcc93680
Douglas Schonholtz <douglas.schonholtz@clearballot.com>
2021-03-12 18:22:17 +0000

    Updated to properly handle different commands for different users

--- install-packages.sh
+++ install-packages.sh
@@ -1,6 +1,6 @@
 # install all of the ppas for the packages we will install later
-apt-get install software-properties-common
-apt-get update
+#apt-get install software-properties-common
+#apt-get update
 while read p; do
   add-apt-repository ppa:"${p}"
 done <ppas.txt
@@ -13,12 +13,12 @@ while read p; do
 done <packages.txt
 
 # install neovim
--u ${logname} mkdir -p ~/.config/nvim/
--u ${logname} cp ./init.vim ~/.config/nvim/
+su - ${LOGNAME} -c "mkdir -p ~/.config/nvim/"
+su - ${LOGNAME} -c "cp ./init.vim ~/.config/nvim/"
 
 #install vim-plug for neovim
--u ${logname} curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+su - ${LOGNAME} -c "curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
 
 # install yarn
--u ${logname} curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+su - ${LOGNAME} -c "curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg" | apt-key add -
 echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
